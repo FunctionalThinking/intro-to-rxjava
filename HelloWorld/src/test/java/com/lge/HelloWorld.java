a99aba47be8212145912fe8ddf7648d07ebad701
Baik Soohyun <soohyun.baik@lge.com>
2015-04-02 16:39:33 +0900

    add simple unit test and settings for junit in gradle.build

---
+++ HelloWorld/src/test/java/com/lge/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lge;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class HelloWorld {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello World", "Hello World");
+    }
+}
